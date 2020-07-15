e3815f1476f473208b2dc743d359d79f0b40aa4c
Sunu Ilham Pradika <sunudika@gmail.com>
2020-07-15 14:15:09 +0700

    Add: Price component

---
+++ src/components/Price.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="font-weight-bold">{{ this.prefix + Number.parseFloat(this.value).toFixed(this.precision) }}</span>
+</template>
+
+<script>
+export default {
+    props: {
+        value: Number,
+        prefix: {
+            type: String,
+            default: '$'
+        },
+        precision: {
+            type: Number,
+            default: 2
+        }
+    },
+}
+</script>
